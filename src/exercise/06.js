dd9cc4f29e2191a51c5c477fb87931e0e7295139
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-05-26 05:20:24 +0200

    ex 6.1 - catch error

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,6 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {
   PokemonForm,
   fetchPokemon,
@@ -15,14 +11,27 @@ import {
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if (!pokemonName) {
       return
     }
     setPokemon(null)
-    fetchPokemon(pokemonName).then(setPokemon)
+    setError(null)
+    fetchPokemon(pokemonName)
+      .then(pokemon => setPokemon(pokemon))
+      .catch(error => setError(error))
   }, [pokemonName])
+
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  }
 
   if (!pokemonName) {
     return 'Submit a pokemon'
